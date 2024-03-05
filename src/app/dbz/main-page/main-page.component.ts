9ab1bbb174a69c8488f21ac1df0f2f977ada6010
Carlos Pupo Garcia <carlos.pupo9601@gmail.com>
2024-03-05 11:16:32 -0500

    OK1

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input } from '@angular/core';
+
+import { Personaje } from '../interfaces/dbz.interfaces';
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+})
+
+export class MainPageComponent {
+
+  @Input() personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 7500
+    }
+  ]
+
+  nuevo: Personaje = {
+    nombre: 'Maestro Roshi',
+    poder: 1000
+  }
+}
